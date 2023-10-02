843434d8a3a51efc149b08d6cb7e29258b256b70
Abdulkhafid001 <you@example.com>
2023-10-02 15:13:56 -0700

    made a critical mistake of creating models in admin instead of models module

--- myshop/votingapp/models.py
+++ myshop/votingapp/models.py
@@ -1,3 +1,19 @@
 from django.db import models
 
 # Create your models here.
+# this module has two models: 1. QuestionModel A Question has a question and a publication date.
+# the Choice has two fields: the text of the choice and a vote tally. Each Choice is associated with a Question.
+class Question(models.Model):
+    # created a char column
+    question_text = models.CharField(max_length=200)
+    # created a datetimefield
+    pub_date = models.DateTimeField("date published")
+
+
+class Choice(models.Model):
+    # the two models are associated using a foreign key 
+    question = models.ForeignKey(Question, on_delete=models.CASCADE)
+    # created choice column
+    choice_text = models.CharField(max_length=200)
+    # created a vote field
+    votes = models.IntegerField(default=0)
