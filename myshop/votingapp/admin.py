2f9de16f0d4c2310f52ae512bd1152c68f1d6e37
Abdulkhafid001 <you@example.com>
2023-10-02 15:21:04 -0700

    fixed the error

--- myshop/votingapp/admin.py
+++ myshop/votingapp/admin.py
@@ -1,3 +1,11 @@
 from django.contrib import admin
 
 # Register your models here.
+
+# steps to register a model
+# Step 1: we have to import the model from our model module into the admin module
+from .models import Question,Choice
+# Step 2: register the model (database table)
+admin.site.register(Question)
+# Step 2: register the model (database table)
+admin.site.register(Choice)
