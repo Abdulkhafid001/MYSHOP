be83b5229189ae261002ddf7c12dc507350218b2
Abdulkhafid001 <you@example.com>
2023-10-05 00:08:04 -0700

    fixed my template bug

--- myshop/mainpollapp/views.py
+++ myshop/mainpollapp/views.py
@@ -1,9 +1,11 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 # import all app models
-from .models import CandidateInfo,ElectionResult
+from .models import CandidateInfo, ElectionResult
 # Create your views here.
+
+
 def indexView(request):
     # from my index view i would get candidate info using a dictionary as a context
     table_content = CandidateInfo.objects.all()
-    return render(request, "index.html", {"canditate" : table_content})
+    return render(request, "index.html", {"candidate": table_content})
