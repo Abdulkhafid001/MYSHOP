7cbb95fa5f65e36aeee5f2dd9c371bdb4ffb8613
Abdulkhafid001 <you@example.com>
2023-10-07 23:36:58 -0700

    finished the minimum viable app

--- myshop/aptechteam/urls.py
+++ myshop/aptechteam/urls.py
@@ -2,5 +2,6 @@ from django.urls import path
 # import view module to invoke our views
 from . import views
 urlpatterns = [
-    path("", views.members, name='aptechteam')
+    path("", views.members, name='aptechteam'),
+    path('playerdetail/<int:player_id>', views.detail_view, name='details')
 ]
--- myshop/aptechteam/views.py
+++ myshop/aptechteam/views.py
@@ -5,12 +5,23 @@ from .models import Members
 # import template loader
 from django.template import loader
 # Create your views here.
+
+
 def members(request):
     # indexpage = loader.get_template("allmembers.html")
-    # get all the data in the model(table) 
+    # get all the data in the model(table)
     team_members = Members.objects.all().values()
     context = {
         # create a dictionary and store the values from the model
-        "team" : team_members,
+        "team": team_members,
     }
     return render(request, "allmembers.html", context)
+
+
+def detail_view(request, player_id):
+    # get player with the id passed as argument to the view in the url
+    player = Members.objects.get(id=player_id)
+    context = {
+        "current_player_data": player,
+    }
+    return render(request, "playerdetail.html", context)
